1e95e03acaf494815e44f02ed9d735c139ede161
Sergey Green <green.sergey@gmail.com>
2021-02-01 06:35:08 +0100

    -

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-git branch | grep -v master | while read b
-do
-    git checkout $b
-    git pull
-    echo > file.txt
-    git commit -am -
-    git commit --allow-empty -am -
-    git push
+git checkout master    
+git branch | grep -v master | while read name
+ do 
+    git push origin --delete $name
+    git branch -D $name
+ done
+ 
+cat branches.txt  | while read b
+do 
+    git checkout -b $b
+    git push --set-upstream origin $b
+    git checkout master
 done
-
-git checkout master
-    
