1ec0f034552c047243f26381f9108057cde7322f
Sergey Green <green.sergey@gmail.com>
2021-02-11 13:31:38 +0100

    -

--- cleanup.sh
+++ cleanup.sh
@@ -10,10 +10,12 @@ git branch | grep -v master | while read name
 cat branches.txt  | while read b
 do 
     git checkout -b $b
-    git commit --allow-empty -am -
+    #git commit --allow-empty -am -
     git log --oneline -1
     touch file_2.txt
     touch $(echo $b | sed -e 'sa/a_a')
+    git add --all
+    git commit -am 'added two files'
     git push --set-upstream origin $b
     git checkout master
 done
