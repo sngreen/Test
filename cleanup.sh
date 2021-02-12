e5d482f19de7f02b6edff76eb62848fecb86a53d
Sergey Green <green.sergey@gmail.com>
2021-02-12 08:22:21 +0100

    -

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# delete existing branches
+
 git checkout master    
-git branch | grep -v master | while read name
+git branch | grep -v master | while read b
  do 
-    git push origin --delete $name
-    git branch -D $name
+    git push origin --delete $b
+    git branch -D $b
  done
- 
+
+# create new branches
+
 cat branches.txt  | while read b
 do 
     git checkout -b $b
@@ -18,3 +22,13 @@ do
     git push --set-upstream origin $b
     git checkout master
 done
+
+# create first commit (at the bottom)
+
+git checkout releases/R5.0K
+git pull
+git checkout -b fix_K/R5.0K
+echo fix_K/R5.0K > file.txt
+git commit -am 'fix_K/R5.0K, first commit'
+git push --set-upstream origin fix_K/R5.0K
+echo create pull request from ix_K/R5.0K to releases/R5.0K
